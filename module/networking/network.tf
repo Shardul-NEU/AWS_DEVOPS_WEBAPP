87a498353566a6b5922ec36580eb285d7bf3d8d5
Shardul-NEU <114615356+Shardul-NEU@users.noreply.github.com>
2023-02-23 17:04:26 -0500

    Ec2 terraform (#6)
    * AWS network setup using terraform
    
    * changes in the workflow file
    
    * updated Readme
    
    * Readme Updated
    
    * updated terraform code
    
    * terraform to provison an ec2 using AMI image
    
    * using variables for port
    
    * retrieves recent ami

--- module/networking/network.tf
+++ module/networking/network.tf
@@ -1,75 +1,76 @@
-// network.tf code
 
-# Create VPC
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = var.vpc_name
+data "aws_ami" "example"{
+  most_recent = true
+  owners = ["892019607445"]
+
+  filter{
+    name = "name"
+    values = ["my-node-app-ami"]
+  }
+
+  filter{
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter{
+    name = "virtualization-type"
+    values = ["hvm"]
   }
 }
-
-# Create public subnets
-resource "aws_subnet" "public" {
-  count                   = length(var.public_subnet_cidr_blocks)
-  cidr_block              = var.public_subnet_cidr_blocks[count.index]
-  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
-  vpc_id                  = aws_vpc.main.id
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.vpc_name}-public-${count.index}"
-  }
+output ec2_ami{
+  value = data.aws_ami.example.id
 }
 
-# Create private subnets
-resource "aws_subnet" "private" {
-  count                   = length(var.private_subnet_cidr_blocks)
-  cidr_block              = var.private_subnet_cidr_blocks[count.index]
-  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
-  vpc_id                  = aws_vpc.main.id
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "${var.vpc_name}-private-${count.index}"
-  }
-}
-
-# Create Internet Gateway
-resource "aws_internet_gateway" "main" {
+# Create security group for web application
+resource "aws_security_group" "app" {
+  name_prefix = "app-"
   vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
-    Name = var.igw_name
+    Name = "Web Application Security Group"
   }
 }
 
-# Create public route table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+resource "aws_instance" "my_instance" {
+  ami = var.ec2_ami
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public[0].id
+  vpc_security_group_ids = [aws_security_group.app.id]
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = 50
+    volume_type = "gp2"
   }
-  tags = {
-    Name = "${var.vpc_name}-public"
-  }
+  disable_api_termination = false
 }
-
-# Create private route table
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.vpc_name}-private"
-  }
-}
-
-# Associate public subnets with public route table
-resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
-
-# Associate private subnets with private route table
-resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
-}
+# code ends here
