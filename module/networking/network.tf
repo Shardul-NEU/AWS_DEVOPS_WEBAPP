5ee64f233b80750c03fc7f6d7c93e11a338fbb42
Shardul-NEU <114615356+Shardul-NEU@users.noreply.github.com>
2023-02-23 18:22:32 -0500

    Ec2 terraform update (#7)
    * AWS network setup using terraform
    
    * changes in the workflow file
    
    * updated Readme
    
    * Readme Updated
    
    * updated terraform code
    
    * terraform to provison an ec2 using AMI image
    
    * using variables for port
    
    * retrieves recent ami
    
    * error resolved

--- module/networking/network.tf
+++ module/networking/network.tf
@@ -1,25 +1,77 @@
+// network.tf code
 
-data "aws_ami" "example"{
-  most_recent = true
-  owners = ["892019607445"]
-
-  filter{
-    name = "name"
-    values = ["my-node-app-ami"]
-  }
-
-  filter{
-    name = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter{
-    name = "virtualization-type"
-    values = ["hvm"]
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = var.vpc_name
   }
 }
-output ec2_ami{
-  value = data.aws_ami.example.id
+
+# Create public subnets
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidr_blocks)
+  cidr_block              = var.public_subnet_cidr_blocks[count.index]
+  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.vpc_name}-public-${count.index}"
+  }
+}
+
+# Create private subnets
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnet_cidr_blocks)
+  cidr_block              = var.private_subnet_cidr_blocks[count.index]
+  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.vpc_name}-private-${count.index}"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+# Create public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "${var.vpc_name}-public"
+  }
+}
+
+# Create private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.vpc_name}-private"
+  }
+}
+
+# Associate public subnets with public route table
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate private subnets with private route table
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
 
 # Create security group for web application
@@ -74,3 +126,5 @@ resource "aws_instance" "my_instance" {
   disable_api_termination = false
 }
 # code ends here
+
+
